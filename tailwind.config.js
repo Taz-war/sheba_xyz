b26101146c611715658a1bb73cb7ac6d373bdcc2
Taz-war <fahimtazwer@gmail.com>
2024-06-26 02:17:15 +0600

    accha

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        'hero-bg':"url('./src/images/hero_bg.webp"
+      }
+    },
+  },
+  corePlugins:{
+    container:false
+  },
+  plugins: [
+    function ({addComponents}) {
+      addComponents({
+        '.container':{
+          maxWidth:'100%',
+          '@screen sm':{
+            maxWidth:'600px',
+          },
+          '@screen md': {
+            maxWidth: '720px',
+          },
+          '@screen lg': {
+            maxWidth: '960px',
+          },
+          '@screen xl': {
+            maxWidth: '1140px',
+          },
+          '@screen 2xl': {
+            maxWidth: '1320px',
+          },
+        }
+      })
+    }
+  ],
+}
+
